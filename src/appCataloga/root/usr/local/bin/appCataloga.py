5818cadaac29b746f700a8333a651f6dab2c3a8b
root <fabioslobao@hotmail.com>
2023-08-17 22:07:43 +0200

    review and test appCataloga

--- src/appCataloga/root/usr/local/bin/appCataloga.py
+++ src/appCataloga/root/usr/local/bin/appCataloga.py
@@ -25,44 +25,47 @@ Listen to socket command to perform backup from a specific host and retuns the c
 
 # Python program to implement server side of chat room.
 import socket
-import select
-import sys
-from _thread import *
+import signal
  
 #constants
-SERVER_IP = "rhzbefipdin01.anatel.gov.br"
-SERVER_PORT = "5555"
+SERVER_IP = "localhost"
+SERVER_PORT = 5555
 QUERY_TAG = "query"
 
 # initialize warning message variable
 warning_msg = ""
 
 #! TEST host_statistics initialization
-host_statistics = { "Total Files":1,
+HOST_STATISTICS = { "Total Files":1,
                     "Files pending backup":1,
                     "Last Backup":"today",
                     "Days since last backup":0}
 
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self, *args):
+    self.kill_now = True
+
+killer = GracefulKiller()
+
+def scheddule_backup(host=["conn","host_id","host_add","user","passwd"]):
+    #nothing
+    print(host)
+    return(HOST_STATISTICS)
+
+print("Starting server")
+
 # set connection
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
- 
 server.bind((SERVER_IP, SERVER_PORT))
- 
 server.listen(500)
- 
-host_list = []
- 
-def backup_thread(host=["conn","host_id","host_add","user","passwd"]):
-    #nothing
-    message = "" 
 
-def get_host_statistics():
-    #nothing
-    message = ""
-
-while True:
- 
+while not killer.kill_now:
     # receive connections
     conn, addr = server.accept()
  
@@ -78,16 +81,10 @@ while True:
     # if first field corresponds to the expected tag, process host indicated
     if host[1] == QUERY_TAG:
 
-        start_new_thread(backup_thread,host)
+        host_statistics = scheddule_backup(host)
         
-        host_statistics = get_host_statistics(host)
         # answer positive
-        message = f'<json>{{"Total Files":{host_statistics["Total Files"]},
-                    "Files pending backup":{host_statistics["Files pending backup"]},
-                    "Last Backup":{host_statistics["Last Backup"]},
-                    "Days since last backup":{host_statistics["Days since last backup"},
-                    "Status":0,
-                    "Message":{warning_msg}}}</json>"
+        message = f'<json>{{"Total Files":{host_statistics["Total Files"]},"Files pending backup":{host_statistics["Files pending backup"]},"Last Backup":{host_statistics["Last Backup"]},"Days since last backup":{host_statistics["Days since last backup"]},"Status":0,"Message":{warning_msg}}}</json>'
     else:
         # answer negative
         message = '<json>{"Status":0,"Message":"Message corrupted or in unrecognized format"}</json>'
@@ -96,3 +93,4 @@ while True:
     conn.close()
 
 server.close()
+print("Clean end")
