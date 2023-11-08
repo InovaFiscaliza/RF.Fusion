f70eb337ce3971d9a31382fd1f2581afbffa3282
Fabio Santos Lobao <fabioslobao@hotmail.com>
2023-11-08 20:01:42 +0100

    Refactor code for better readability and performance.

--- src/appCataloga/root/usr/local/bin/appCataloga/processing_control.py
+++ src/appCataloga/root/usr/local/bin/appCataloga/processing_control.py
@@ -25,6 +25,10 @@ sys.path.append('/etc/appCataloga')
 
 from fastcore.xtras import Path
 from rfpye.utils import get_files
+from rfpye.parser import parse_bin
+
+from rfpye import parse_bin
+from rich import print
 
 # Import libraries for file processing
 import pandas as pd
@@ -40,40 +44,48 @@ import config as k
 import db_handler as dbh
 import shared as sh
 
-class BinFileHandler:
+# create a warning message object
+log = sh.log(verbose=True, target_screen=True, target_file=True)
 
-    def __init__(self, fileFullPath):
+# create db object using databaseHandler class for the backup and processing database
+db_bkp = dbh.dbHandler(database=k.BKP_DATABASE_NAME)
+db_rfm = dbh.dbHandler(database=k.RFM_DATABASE_NAME)
 
-        # store reference infortion to the file
+class BinProcessing:
 
-        # use rfpye to extract metadata from the file
-        rfpyeDict = extract_bin_data(path = fileFullPath)
+    def __init__(self, task):
 
-        # clean out the outer dictionary form rfpye 
-        # ! This script process only single files
-        self.metadata = rfpyeDict[filename]
-        self.metadata['fileFullPath'] = fileFullPath
-
-        # perform adjustments from the file path to the desired URL
-        targetPath = fileFullPath.replace(k.FOLDER_TO_WATCH,k.TARGET_ROOT_URL)
-        targetPath = targetPath.replace('\\','/')
-        self.metadata['URL'] = targetPath
+        filename = f"{task['server path']}\{task['server file']}"
+        # store reference infortion to the file        
+        try:
+            self.bin_metadata = parse_bin(filename)
+        except:
+            # TODO: handle error
+            log.error(f"Error parsing file {filename}")
+        
+        # merge the two dictionaries
+        self.metadata = {**task, **self.bin_metadata}
 
     # recursive function to perform several tries in geocoding before final time out.
     def doReveseGeocode(self, attempt=1, max_attempts=10):
 
+        # try to find the location data in the database
+        
+        
         # find location data references using the open free service of Nominatim - https://nominatim.org/ 
         point = (self.metadata['Latitude'],self.metadata['Longitude'])
         geocodingService = Nominatim(user_agent=k.NOMINATIM_USER, timeout = 5)
 
         # try using service with extended timeout and increasing up to 15 seconds and delays of 2 seconds between consecutive attempts
-        try:
-            location = geocodingService.reverse(point, timeout = 5+attempt, language="pt")
-        except GeocoderTimedOut:
-            if attempt <= max_attempts:
-                time.sleep(2)
-                location = self.doReveseGeocode(point, attempt=attempt+1)
-            raise
+        attempt = 1
+        while attempt <= k.MAX_NOMINATIN_ATTEMPTS:
+            try:
+                location = geocodingService.reverse(point, timeout = 5+attempt, language="pt")
+            except GeocoderTimedOut:
+                if attempt <= max_attempts:
+                    time.sleep(2)
+                    location = self.doReveseGeocode(point, attempt=attempt+1)
+                raise
 
         # populate location data with information from the geocoding result.
         # Loop through all required fields as defined in the constat, and get data from dictionary using associated nominatim semantic translations
@@ -127,128 +139,29 @@ class BinFileHandler:
 
 # function that performs the file processing
 def file_processing(task):
-    
+    print("nada")
 
 def main():
-    
-    # create a warning message object
-    log = sh.log(verbose=True, target_screen=True, target_file=True)
-    
-    # create db object using databaseHandler class for the backup and processing database
-    db = dbh.dbHandler(database=k.BKP_DATABASE_NAME)
-
+        
     while True:
         # Get one backup task from the queue in the database
-        task = db.next_backup_task()
+        task = db_bkp.next_processing_task()
 
-        """	
-            task={  "task_id": str,
-                    "host_id": str,
-                    "host_add": str,
-                    "port": str,
-                    "user": str,
-                    "password": str}"""        
-        
         # if there is a task in the database
         if task:
             # check if there is a task already running for the same host and remove it if it is the case, avoiding the creation of multiple tasks for the same host
-            new_task = True
-            for running_task in tasks:
-                if running_task["host_id"] == task["host_id"]:
-                    db.remove_backup_task(task)
-                    new_task = False
+            # get metadata from bin file
+            bin_data = BinProcessing(task)
             
-            # if it really is a new task and the total number of tasks running didn't top the capacity alloted
-            if new_task and (task_counter < k.BKP_MAX_PROCESS):
-                log.entry(f"Adding backup task for {task['host_add']}.")
+            bin_data.do_reveseGeocode()
 
-                command = ( f'bash -c '
-                            f'"source ~/miniconda3/etc/profile.d/conda.sh; '
-                            f'conda activate appdata; '
-                            f'python3 {k.BACKUP_SINGLE_HOST_MODULE} '
-                            f'host_id={task["host_id"]} '
-                            f'host_add={task["host_add"]} '
-                            f'port={task["port"]} '
-                            f'user={task["user"]} '
-                            f'pass={task["password"]}"')
-                
-                # add task to tasks list
-                task["process_handle"] = subprocess.Popen([command],
-                                                        stdout=subprocess.PIPE,
-                                                        stderr=subprocess.PIPE,
-                                                        text=True,
-                                                        shell=True)
-                
-                task["birth_time"] = time.time()
-                task["nu_backup_error"] = 0
-                tasks.append(task)                
-                task_counter += 1 
-                          
-                # remove task from database
-                db.remove_backup_task(task)
+            # update database with metadata
+            db.update_processing_status(BinFile.metadata)
+        
+            # TODO: Implement error handling to rollback partial database updates                
+            dbHandle.updateDatabase(BinFile)
 
-        # if there are tasks running
-        if len(tasks) > 0:
-            # loop through tasks list and remove completed tasks
-            for running_task in tasks:
-                # Get output from the running task, wainting 5 seconds for timeout
-                try:
-                    task_output, task_error = running_task["process_handle"].communicate(timeout=5)
-                except subprocess.TimeoutExpired:
-                    task_output = False
-                    task_error = False
-                    # in case of timeout, check if the task is running for more than the alotted time and remove it if it is the case, otherwise, pass
-                    if time.time() - running_task["birth_time"] > k.BKP_TASK_EXECUTION_TIMEOUT:
-                        running_task["process_handle"].kill()
-                        tasks.remove(running_task)
-                        log.warning(f"Backup task canceled due to timeout {task['host_add']}")
-                    pass
-                
-                # if there is output from the task, process it
-                if task_output:
-                    try:
-                        task_dict_output = json.loads(task_output)
-                        
-                        # remove task from tasks list
-                        tasks.remove(running_task)
-                            
-                        # add the list of files to the processing task list TODO: if len(task_dict_output['done_backup_list']) > 0:
-                        db.add_processing_task(hostid=task_dict_output['host_id'],
-                                            done_backup_list=task_dict_output['done_backup_list'])
-            
-                        # update backup summary status for the host_id in case of previous errors
-                        task_dict_output['nu_backup_error'] = task_dict_output['nu_backup_error'] + running_task['nu_backup_error']
-                        
-                        # update backup summary status for the host_id
-                        db.update_backup_status(task_dict_output)
-                        
-                        log.entry(f"Completed backup from {running_task['host_add']}")
-                        
-                    except json.JSONDecodeError as e:
-                        log.error(f"Malformed JSON received. Dumped\n***Output: {task_output}\n***Error: {e}")
-                    
-                elif task_error:
-                    running_task["nu_backup_error"] += 1
-
-                    log.entry(f"Error in backup from {task['host_add']}. Will try again later. Error: {task_error}")
-                    
-                # if birth time is more than alowed time, remove task from tasks list
-                execution_time = time.time() - running_task["birth_time"]
-                if  execution_time > k.BKP_TASK_EXECUTION_TIMEOUT:
-                    
-                    # kill the process
-                    running_task["process_handle"].kill()
-                    
-                    # remove task from tasks list
-                    tasks.remove(running_task)
-                        
-                    # remove task from database. If there are pending backup, it will be consider in the next cycle.
-                    db.remove_backup_task(running_task)
-
-                    failed_task["nu_backup_error"] = running_task["nu_backup_error"] + 1
-
-                    # update backup summary status for the host_id
-                    db.update_backup_status(failed_task)
+            self.fileMove(event, BinFileMetadata.newFilePath, BinFileMetadata.FileName)
                     
                     log.warning(f"Backup task killed due to timeout for host {task['host_add']} after {execution_time/60} minutes.")
             
