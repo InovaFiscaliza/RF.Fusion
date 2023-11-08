6d07c153e32df93a284663c4c6ba911eecc14a13
Fabio Santos Lobao <fabioslobao@hotmail.com>
2023-11-08 20:01:27 +0100

    Refactor code for improved readability and performance.

--- src/appCataloga/root/usr/local/bin/appCataloga/createMeasureDB.sql
+++ src/appCataloga/root/usr/local/bin/appCataloga/createMeasureDB.sql
@@ -11,37 +11,80 @@ CREATE TABLE DIM_EQUIPAMENTO_TIPO (
     PRIMARY KEY (ID_TIPO_EQUIPAMENTO)
 );
 
-CREATE TABLE DIM_MEDICAO_ESPECTRO_EQUIPAMENTO (
+CREATE TABLE DIM_SPECTRUN_EQUIPAMENTO (
     ID_EQUIPAMENTO INT NOT NULL AUTO_INCREMENT, -- Primary Key to equipment dimension table --
     NO_EQUIPAMENTO VARCHAR(50), -- Unique identifier to the equipment - physical identification --
-    ID_TIPO_EQUIPAMENTO INT, -- Foreign key to equipment type table -- 
+    FK_TIPO_EQUIPAMENTO INT, -- Foreign key to equipment type table -- 
     FOREIGN KEY (ID_TIPO_EQUIPAMENTO) REFERENCES DIM_EQUIPAMENTO_TIPO(ID_TIPO_EQUIPAMENTO),
     PRIMARY KEY (ID_EQUIPAMENTO)
 );
 
 -- MEASUREMENT LOCATION DIMENSION
-CREATE TABLE DIM_LOCALIZACAO_UF (
-    ID_UF_IBGE INT NOT NULL, -- Primary Key to file UF dimension table --
-    NO_UF VARCHAR(50), -- Location State full name --
-    SG_UF VARCHAR(2), -- Location State short form --
-    PRIMARY KEY (ID_UF_IBGE)
+CREATE TABLE DIM_SITE_STATE (
+    ID_STATE_CODE INT NOT NULL, -- Primary Key to file STATE dimension table --
+    NO_STATE VARCHAR(50), -- Location State full name --
+    SG_STATE VARCHAR(2), -- Location State short form --
+    PRIMARY KEY (ID_STATE_CODE)
 );
 
-CREATE TABLE DIM_LOCALIZACAO_MUNICIPIO (
-    ID_MUNICIPIO_IBGE INT NOT NULL, -- Primary Key to file type dimension table --
-    ID_UF_IBGE INT, -- Foreign key to location state table --
-    NO_MUNICIPIO NVARCHAR(60), -- Location County full name  --
-    PRIMARY KEY (ID_MUNICIPIO_IBGE),
-    CONSTRAINT FK_MUNICIPIO_UF FOREIGN KEY (ID_UF_IBGE) REFERENCES DIM_LOCALIZACAO_UF (ID_UF_IBGE)
+CREATE TABLE DIM_SITE_COUNTY (
+    ID_COUNTY_CODE INT NOT NULL, -- Primary Key to file type dimension table --
+    FK_STATE_CODE INT, -- Foreign key to location state table --
+    NO_COUNTY NVARCHAR(60), -- Location County full name  --
+    PRIMARY KEY (ID_COUNTY_CODE),
+    CONSTRAINT FK_COUNTY_STATE FOREIGN KEY (ID_STATE_CODE) REFERENCES DIM_SITE_STATE (ID_STATE_CODE)
 );
 
-CREATE TABLE DIM_LOCALIZACAO_DISTRITO (
-    ID_DISTRITO INT NOT NULL AUTO_INCREMENT, -- Primary Key to file type dimension table --
-    ID_MUNICIPIO_IBGE INT, -- Foreign key to location municipality table --
-    NO_DISTRITO NVARCHAR(50), -- Location County full name  --
-    PRIMARY KEY (ID_DISTRITO),
-    CONSTRAINT FK_DISTRITO_MUNICIPIO FOREIGN KEY (ID_MUNICIPIO_IBGE) REFERENCES DIM_LOCALIZACAO_MUNICIPIO (ID_MUNICIPIO_IBGE)
+CREATE TABLE DIM_SITE_DISTRICT (
+    ID_DISTRICT INT NOT NULL AUTO_INCREMENT, -- Primary Key to file type dimension table --
+    FK_COUNTY_CODE INT, -- Foreign key to location municipality table --
+    NO_DISTRICT NVARCHAR(50), -- Location County full name  --
+    PRIMARY KEY (ID_DISTRICT),
+    CONSTRAINT FK_DISTRICT_COUNTY FOREIGN KEY (ID_COUNTY_CODE) REFERENCES DIM_SITE_COUNTY (ID_COUNTY_CODE)
 );
+
+CREATE TABLE DIM_SPECTRUN_SITE (
+    ID_SITE INT NOT NULL IDENTITY(1,1) PRIMARY KEY, -- Primary Key to site location dimension table --
+    PK_SITE_DISTRICT INT FOREIGN KEY REFERENCES DIM_SITE_DISTRICT(ID_DISTRICT), -- Foreign key to District table Not related to County to handle case where there is no District -- 
+    PK_COUNTY_CODE INT FOREIGN KEY REFERENCES DIM_SITE_COUNTY(ID_COUNTY_CODE), -- Foreign key to County table. Not related to State to handle case where there is no County -- 
+    PK_STATE_CODE INT FOREIGN KEY REFERENCES DIM_SITE_UF(ID_UF_CODE), -- Foreign key to UF table -- 
+    NO_SITE NVARCHAR(100), -- Location description. Future use for easier referencing to location common name. 
+    GEOLOCATION POINT NULL, -- Location with longitude, latitude and altitude (Z-=10.3) with 0 for measure (M)- POINT(-122.34900 47.65100 10.3 0)
+    NU_GNSS_MEASUREMENTS BIGINT NULL, -- Number of GNSS measurements used for the definition of the site location  --
+);
+
+    -- create spatial index to allow for spatial queries
+    CREATE SPATIAL INDEX SP_INDEX on DIM_SPECTRUN_SITE(GEOLOCATION)
+
+    -- Create full text catalogs used create fulltext search indexes 
+        -- Language 1046 = Português (Brasil) / Alternative 0 = Neutral
+        -- https://www.sqlshack.com/hands-full-text-search-sql-server/ TYPE COLUMN OBJ_FILE_IDX_DOCTYPE 
+
+    CREATE FULLTEXT CATALOG FullTextCatalog AS DEFAULT;
+
+    -- Do not create a fulltext search for State. Due to the small number of entries, use LIKE statement in the select
+
+    -- Create fulltext search indexes for UF
+        CREATE FULLTEXT
+        INDEX ON  DIM_SITE_UF (NO_UF LANGUAGE 1046)
+        KEY INDEX ID_BUSCA_TEXTUAL_UF WITH 
+            CHANGE_TRACKING = AUTO, 
+            STOPLIST=OFF;
+
+    -- Create fulltext search indexes for county
+        CREATE FULLTEXT
+        INDEX ON  DIM_SITE_COUNTY (NO_COUNTY LANGUAGE 1046)
+        KEY INDEX ID_BUSCA_TEXTUAL_COUNTY WITH 
+            CHANGE_TRACKING = AUTO, 
+            STOPLIST=OFF;
+
+    -- Create fulltext search indexes for district
+        CREATE FULLTEXT
+        INDEX ON  DIM_SITE_DISTRICT (NO_DISTRICT LANGUAGE 1046)
+        KEY INDEX ID_BUSCA_TEXTUAL_DISTRICT WITH 
+            CHANGE_TRACKING = AUTO, 
+            STOPLIST=OFF;
+
 
 -- MEASUREMENT FILE DIMENSION
 CREATE TABLE DIM_ARQUIVO_TIPO (
@@ -49,7 +92,7 @@ CREATE TABLE DIM_ARQUIVO_TIPO (
     NO_TIPO_ARQUIVO NVARCHAR(50) -- Description of the file type --
 );
 
-CREATE TABLE DIM_MEDICAO_ESPECTRO_ARQUIVO (
+CREATE TABLE DIM_SPECTRUN_ARQUIVO (
     ID_ARQUIVO INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Primary Key to file dimension table --
     ID_TIPO_ARQUIVO INT, -- Foreign key to file type table -- 
     NO_ARQUIVO NVARCHAR(100), -- Unique identifier to the filename --
@@ -58,23 +101,23 @@ CREATE TABLE DIM_MEDICAO_ESPECTRO_ARQUIVO (
 );
 
 -- MEASUREMENT PARAMETERS DIMENSION
-CREATE TABLE DIM_MEDICAO_ESPECTRO_DETECTOR (
+CREATE TABLE DIM_SPECTRUN_DETECTOR (
     ID_TIPO_DETECTOR INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Primary Key to measurement detector type dimension table -- 
     NO_TIPO_DETECTOR NVARCHAR(50) -- Description of the measurement detector type. e.g. RMS, Sample, Positive Peak --
 );
 
-CREATE TABLE DIM_MEDICAO_ESPECTRO_TRACO (
+CREATE TABLE DIM_SPECTRUN_TRACO (
     ID_TIPO_TRACO INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Primary Key to measurement trace type dimension table -- 
     NO_TIPO_TRACO NVARCHAR(50) -- Description of the measurement trace type. e.g. Peak, Average, Minimum --
 );
 
-CREATE TABLE DIM_MEDICAO_ESPECTRO_UNIDADE (
+CREATE TABLE DIM_SPECTRUN_UNIDADE (
     ID_UNIDADE_MEDIDA INT NOT NULL PRIMARY KEY, -- Primary Key to measurement trace type dimension table -- 
     NO_UNIDADE_MEDIDA NVARCHAR(10) -- Description of the measurement trace type. e.g. Peak, Average, Minimum --
 );
 
 -- DRAFT TO FEATURE IN BACKLOG - MEASURED EMITTERS DIMENSION
-CREATE TABLE DIM_MEDICAO_ESPECTRO_EMISSOR (
+CREATE TABLE DIM_SPECTRUN_EMISSOR (
     ID_EMISSOR INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Primary Key to emitter dimension table -- 
     NO_EMISSOR NVARCHAR(50) -- Unique identifier to emitter in the spectrum management database --
 );
@@ -85,14 +128,14 @@ CREATE TABLE DIM_MEDICAO_PROCEDIMENTO (
 );
 
 -- MEASUREMENT DATA FACT
-CREATE TABLE FATO_MEDICAO_ESPECTRO (
-    ID_FATO_MEDICAO_ESPECTRO INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Primary Key to the fact table of spectrum measurement data. --
-    ID_ARQUIVO INT, -- Foreign key to file dimension table --
-    ID_LOCALIZACAO INT, -- Foreign key to location dimension table --
-    ID_TIPO_DETECTOR INT, -- Foreign key to location dimension table --
-    ID_TIPO_TRACO INT, -- Foreign key to location dimension table --
-    ID_UNIDADE_MEDIDA INT, -- Foreign key to location dimension table --
-    ID_PROCEDIMENTO INT, -- Foreign key to measurement procedure used --
+CREATE TABLE FATO_SPECTRUN (
+    ID_FATO_SPECTRUN INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Primary Key to the fact table of spectrum measurement data. --
+    FK_ARQUIVO INT, -- Foreign key to file dimension table --
+    FK_SITE INT, -- Foreign key to location dimension table --
+    FK_TIPO_DETECTOR INT, -- Foreign key to location dimension table --
+    FK_TIPO_TRACO INT, -- Foreign key to location dimension table --
+    FK_UNIDADE_MEDIDA INT, -- Foreign key to location dimension table --
+    FK_PROCEDIMENTO INT, -- Foreign key to measurement procedure used --
     NO_DESCRIPTION NVARCHAR(3000), -- Text description for the measurement --
     NU_FREQUENCIA_INICIAL DECIMAL(14,6) NULL, -- Initial frequency expressed in MHz --
     NU_FREQUENCIA_FINAL DECIMAL(14,6) NULL, -- End frequency expressed in MHz --
@@ -107,17 +150,17 @@ CREATE TABLE FATO_MEDICAO_ESPECTRO (
 );
 
 -- BRIDGE TABLES TO ALLOW FOR N-N RELATIONSHIPS TO THE FACT TABLE
-CREATE TABLE PONTE_MEDICAO_ESPECTRO_EQUIPAMENTO (
+CREATE TABLE PONTE_SPECTRUN_EQUIPAMENTO (
     ID_PONTE_EQUIPAMENTO INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Primary Key to bridge table to equipment dimension table. Allows multiple equipment to be associated with a single measurement and many measurements to be associated with the same equipment -- 
-    ID_EQUIPAMENTO INT, -- Foreign key to equipment table -- 
-    ID_MEDICAO_ESPECTRO INT -- Foreign key to spectrum measurement table -- 
+    FK_EQUIPAMENTO INT, -- Foreign key to equipment table -- 
+    FK_SPECTRUN INT -- Foreign key to spectrum measurement table -- 
 );
 
 -- BRIDGE TABLES TO ALLOW FOR N-N RELATIONSHIPS TO THE FACT TABLE
-CREATE TABLE PONTE_MEDICAO_ESPECTRO_EMISSOR (
+CREATE TABLE PONTE_SPECTRUN_EMISSOR (
     ID_PONTE_EMISSOR INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Primary Key to bridge table to equipment dimension table. Allows multiple equipment to be associated with a single measurement and many measurements to be associated with the same equipment -- 
-    ID_EMISSOR INT, -- Foreign key to equipment table -- 
-    ID_MEDICAO_ESPECTRO INT -- Foreign key to spectrum measurement table -- 
+    FK_EMISSOR INT, -- Foreign key to equipment table -- 
+    FK_SPECTRUN INT -- Foreign key to spectrum measurement table -- 
 );
 
 -- Data Uploads
@@ -130,25 +173,25 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (ID_TIPO_EQUIPAMENTO, NO_TIPO_EQUIPAMENTO);
 
--- Upload Data to DIM_LOCALIZACAO_UF
-LOAD DATA INFILE '/etc/appCataloga/IBGE-BR_UF_2020_BULKLOAD.csv'
-INTO TABLE DIM_LOCALIZACAO_UF
+-- Upload Data to DIM_SITE_STATE
+LOAD DATA INFILE '/etc/appCataloga/CODE-BR_STATE_2020_BULKLOAD.csv'
+INTO TABLE DIM_SITE_STATE
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES
-(ID_UF_IBGE, NO_UF, SG_UF);
+(ID_STATE_CODE, NO_STATE, SG_STATE);
 
--- Upload Data to DIM_LOCALIZACAO_MUNICIPIO
-LOAD DATA INFILE '/etc/appCataloga/IBGE-BR_Municipios_2020_BULKLOAD.csv'
-INTO TABLE DIM_LOCALIZACAO_MUNICIPIO
+-- Upload Data to DIM_SITE_COUNTY
+LOAD DATA INFILE '/etc/appCataloga/CODE-BR_Municipios_2020_BULKLOAD.csv'
+INTO TABLE DIM_SITE_COUNTY
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES
-(ID_MUNICIPIO_IBGE, ID_UF_IBGE, NO_MUNICIPIO);
+(ID_COUNTY_CODE, ID_STATE_CODE, NO_COUNTY);
 
--- Upload Data to DIM_MEDICAO_ESPECTRO_UNIDADE
+-- Upload Data to DIM_SPECTRUN_UNIDADE
 LOAD DATA INFILE '/etc/appCataloga/measurementUnit.csv'
-INTO TABLE DIM_MEDICAO_ESPECTRO_UNIDADE
+INTO TABLE DIM_SPECTRUN_UNIDADE
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES
