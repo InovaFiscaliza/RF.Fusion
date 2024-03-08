b746d09091fb779cfe539cb63e9e1382ac25bea5
Fabio Santos Lobao <fabioslobao@hotmail.com>
2024-03-08 15:22:32 +0100

    correct html parsing for elsys missing items

--- src/zabbix/templates/APC/elsys_status.js
+++ src/zabbix/templates/APC/elsys_status.js
@@ -1,56 +1,64 @@
 // Convert APC HTML to JSON
 
 // load the HTML content from a file - test only
-// var value = require('fs').readFileSync('/root/RF.Fusion/src/zabbix/templates/APC/elsys_status.html', 'utf8');
+//var value = require('fs').readFileSync('/root/RF.Fusion/src/zabbix/templates/APC/status.html', 'utf8');
 
 // Split the value into substrings and put these substrings into an array
 var html_input = value.split('\n');
 
 // Create list of variables to recover from UPS Status Page
 var parameter_output = {
-    'ip_address': "",
-    'nivel_sinal': "",
-    'nivel_sinal_percent': "",
-    'nomeOperadoraNotParsed': "",
-    'tecnologia': "",
-    'banda': "",
-    'freq_3G': "",
-    'imei': "",
-    'tipo_rede_movel': "",
-    'tipo_de_antena': "",
-    'levelQuality': "",
-    'iccid': "",
-    'wan_address': ""
+    'ip_address': "ND",	
+    'nivel_sinal': "0 dBm",
+    'nivel_sinal_percent': "0 %",
+    'nomeOperadoraNotParsed': "ND",
+    'tecnologia': "ND",
+    'banda': "0 MHz",
+    'freq_3G': "0 MHz",
+    'imei': "ND",
+    'tipo_rede_movel': "ND",
+    'tipo_de_antena': "ND",
+    'levelQuality': "0 dB",
+    'iccid': "ND",
+    'wan_address': "ND"
 };
 
 var parameter_list = Object.keys(parameter_output);
 
-var parameter_index = 0;
-var line = 0;
 // Loop through the "html_input" array
-while (parameter_index < parameter_list.length) {
-    line++;
+for (var line = 0; line < html_input.length; line++) {
     
-    var key_match = html_input[line].match(parameter_list[parameter_index]);
+    for (var i = 0; i < parameter_list.length; i++) {
+        
+        var key_match = html_input[line].match(parameter_list[i]);
 
-    // If the parameter is found, use match() with a regular expression to find the value in the following line
-    if (key_match !== null) {
-        // replace " with ' to avoid problems with parsing
-        html_input[line] = html_input[line].replace(/"/g, "'");
-        html_input[line] = html_input[line].replace(/''/g, "'");
+        // If the parameter is found, use match() with a regular expression to find the value in the following line
+        if (key_match !== null) {
+            // replace " with ' to avoid problems with parsing
+            html_input[line] = html_input[line].replace(/"/g, "'");
+            html_input[line] = html_input[line].replace(/''/g, "'");
 
-        // use match() to find the parameter value enclosed by ' in the line
-        var parameter_value = html_input[line].match(/'.*'/)[0] || "";
+            // use match() to find the parameter value enclosed by ' in the line
+            var parameter_value = html_input[line].match(/'.*'/)[0] || "";
 
-        parameter_output[parameter_list[parameter_index]] = parameter_value.slice(1, -1);
-        parameter_index++;
+            parameter_output[parameter_list[i]] = parameter_value.slice(1, -1);
+            
+            // remove the parameter from the list
+            parameter_list.splice(i, 1);
+
+            break;
+        }
+    }
+
+    if (parameter_list.length == 0) {
+        break;
     }
 }
 
 var output = JSON.stringify(parameter_output);
 
 // print the output for testing
-// console.log(output);
+//console.log(output);
 
 // Return JSON string
 return output;
