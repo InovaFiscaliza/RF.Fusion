aea6fd6ad9893681f4c238bf343580e98daaea06
Fabio Santos Lobao <fabioslobao@hotmail.com>
2024-04-04 11:15:16 +0200

    Quick service start and stop

---
+++ src/appCataloga/start_services.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+systemctl start run_host_task.service
+echo "run_host_task.service started"
+
+systemctl start run_file_backup_task@0.service 
+echo "run_file_backup_task@0.service started"
+
+systemctl start run_file_bin_processing.service
+echo "run_file_bin_processing.service started"
+
+systemctl start appCataloga.service
+echo "appCataloga.service started"
---
+++ src/appCataloga/stop_services.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+systemctl stop run_host_task.service
+echo "run_host_task.service stopped"
+
+# Create a list with all instances of run_file_backup_task@* services
+# and stop them
+for i in $(systemctl list-units --full --all | grep run_file_backup_task@ | awk '{print $1}'); do
+    systemctl stop $i
+    echo "$i stopped"
+done
+
+systemctl stop run_file_bin_processing.service
+echo "run_file_bin_processing.service stopped"
+
+systemctl stop appCataloga.service
+echo "appCataloga.service stopped"
+
+read -p "Remove log file? [y/N] " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Log file not removed"
+else
+    rm -f /var/log/appCataloga.log
+    echo "Log file removed"
+fi
+
